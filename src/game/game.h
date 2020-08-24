4e70867e05afca5347201ba44cea856a61bb59c7
weremsoft <pablo.weremczuk@gmail.com>
2020-08-25 00:30:42 +0200

    [ADD] castling implemented for white. No invalid move

--- src/game/game.h
+++ src/game/game.h
@@ -17,6 +17,9 @@ enum game_state_enum {
     GAME_STATE_ANIMATING,
     GAME_STATE_WON_WHITE,
     GAME_STATE_WON_BLACK,
+    GAME_STATE_CASTLING_WHITE_RIGHT,
+    GAME_STATE_CASTLING_WHITE_LEFT,
+    GAME_STATE_CASTLING_BLACK,
 };
 
 typedef struct game_context_t 
@@ -35,6 +38,8 @@ typedef struct game_context_t
     char target_position[5];
     Vector3 piece_to_move_position_actual;
     Vector3 piece_to_move_position_target;
+    Vector3 piece_castling_position_actual;
+    Vector3 piece_castlint_position_target;
     float piece_to_move_lerp_percentage;
     unsigned int piece_to_move;
 } game_context_t;
@@ -56,6 +61,34 @@ void game_pass_to_state_animating(game_context_t* game_context, char* position){
     game_context->piece_to_move_position_target = game_board_get_target_coordinates_in_world(position);
     game_board_set_piece_at_source(game_context->board, position, PIECE_IN_MOTION);
     strncpy(game_context->target_position, position, 5);
+}
+
+void game_pass_to_state_castling_white_right(game_context_t* game_context){
+    game_context->state = GAME_STATE_CASTLING_WHITE_RIGHT;
+    game_context->piece_to_move_lerp_percentage = 0;
+
+    game_context->piece_to_move_position_actual = game_board_get_source_coordinates_in_world("e1xx");
+    game_context->piece_to_move_position_target = game_board_get_target_coordinates_in_world("xxg1");
+
+    game_context->piece_castling_position_actual = game_board_get_source_coordinates_in_world("h1xx");
+    game_context->piece_castlint_position_target = game_board_get_target_coordinates_in_world("xxf1");
+
+    game_board_set_piece_at_source(game_context->board, "e1xx", PIECE_IN_MOTION);
+    game_board_set_piece_at_source(game_context->board, "f1xx", PIECE_IN_MOTION);
+}
+
+void game_pass_to_state_castling_white_left(game_context_t* game_context){
+    game_context->state = GAME_STATE_CASTLING_WHITE_LEFT;
+    game_context->piece_to_move_lerp_percentage = 0;
+
+    game_context->piece_to_move_position_actual = game_board_get_source_coordinates_in_world("e1xx");
+    game_context->piece_to_move_position_target = game_board_get_target_coordinates_in_world("xxc1");
+
+    game_context->piece_castling_position_actual = game_board_get_source_coordinates_in_world("a1xx");
+    game_context->piece_castlint_position_target = game_board_get_target_coordinates_in_world("xxd1");
+
+    game_board_set_piece_at_source(game_context->board, "e1xx", PIECE_IN_MOTION);
+    game_board_set_piece_at_source(game_context->board, "a1xx", PIECE_IN_MOTION);
 }
 
 void game_event_process(game_context_t* game_context){
@@ -90,19 +123,31 @@ void game_event_process(game_context_t* game_context){
             break;
             case EVENT_COMMAND:
                 {
-                    game_pass_to_state_animating(game_context, event.data);
 
                     char command_as_string[300] = {0};
-
                     snprintf(command_as_string, 
-                        300, 
-                        "position startpos moves%s %s",
-                        command_history_get_buffer(),
-                        event.data);
-
+                    300, 
+                    "position startpos moves%s %s",
+                    command_history_get_buffer(),
+                    event.data);
                     parse_line(command_as_string);
+
+                    if(strcmp(event.data, "e1g1") == 0
+                        && TWR_W == game_board_get_piece_at_source(game_context->board, "h1xx")
+                        && EMPTY == game_board_get_piece_at_source(game_context->board, "f1xx")){
+                        game_pass_to_state_castling_white_right(game_context);
+                    } else if(strcmp(event.data, "e1c1") == 0
+                        && TWR_W == game_board_get_piece_at_source(game_context->board, "a1xx")
+                        && EMPTY == game_board_get_piece_at_source(game_context->board, "b1xx")
+                        && EMPTY == game_board_get_piece_at_source(game_context->board, "c1xx")
+                        && EMPTY == game_board_get_piece_at_source(game_context->board, "d1xx")
+                        ){
+                        game_pass_to_state_castling_white_left(game_context);
+                    } else {
+                        game_pass_to_state_animating(game_context, event.data);
+                    }
+
                     parse_line(commands[GO]);
-                    
                     command_history_add_command(" ");
                     command_history_add_command(event.data);
                 }
@@ -218,6 +263,77 @@ void game_update(game_context_t* game_state)
                 game_board_draw(game_state->board);
                 game_board_pieces_draw(game_state->piece_to_move, game_state->piece_to_move_position_actual);
                 selector_draw(game_state->selector);
+            }
+            EndMode3D();
+            game_draw_post(game_state);
+            break;
+
+        case GAME_STATE_CASTLING_WHITE_RIGHT:
+            game_state->piece_to_move_lerp_percentage += 0.01;
+            game_state->piece_to_move_position_actual = Vector3Lerp(game_state->piece_to_move_position_actual, game_state->piece_to_move_position_target, game_state->piece_to_move_lerp_percentage);
+            game_state->piece_castling_position_actual = Vector3Lerp(game_state->piece_castling_position_actual, game_state->piece_castlint_position_target, game_state->piece_to_move_lerp_percentage);
+            if(Vector3Distance(game_state->piece_to_move_position_actual, game_state->piece_to_move_position_target) < 0.1){
+                    
+                    game_state->piece_to_move_position_actual = game_state->piece_to_move_position_target;
+                    game_board_set_piece_at_target(game_state->board, "xxg1", KNG_W);
+                    game_board_set_piece_at_source(game_state->board, "e1xx", EMPTY);
+                    game_board_set_piece_at_target(game_state->board, "xxf1", TWR_W);
+                    game_board_set_piece_at_source(game_state->board, "h1xx", EMPTY);
+
+                    game_state->state = GAME_STATE_PLAYING;
+            }
+            game_draw_pre();
+
+            ClearBackground(LIGHTGRAY);
+            DrawTexture(game_state->background, 0, 0, WHITE);
+            
+            BeginMode3D(game_state->camera_perspective);
+            {
+                game_board_draw(game_state->board);
+                game_board_pieces_draw(KNG_W, game_state->piece_to_move_position_actual);
+                game_board_pieces_draw(TWR_W, game_state->piece_castling_position_actual);
+            }
+            EndMode3D();
+
+            BeginMode3D(game_state->camera_top);
+            {
+                game_board_draw(game_state->board);
+                game_board_pieces_draw(game_state->piece_to_move, game_state->piece_to_move_position_actual);
+            }
+            EndMode3D();
+            game_draw_post(game_state);
+            break;
+        case GAME_STATE_CASTLING_WHITE_LEFT:
+            game_state->piece_to_move_lerp_percentage += 0.01;
+            game_state->piece_to_move_position_actual = Vector3Lerp(game_state->piece_to_move_position_actual, game_state->piece_to_move_position_target, game_state->piece_to_move_lerp_percentage);
+            game_state->piece_castling_position_actual = Vector3Lerp(game_state->piece_castling_position_actual, game_state->piece_castlint_position_target, game_state->piece_to_move_lerp_percentage);
+            if(Vector3Distance(game_state->piece_to_move_position_actual, game_state->piece_to_move_position_target) < 0.1){
+                    
+                    game_state->piece_to_move_position_actual = game_state->piece_to_move_position_target;
+                    game_board_set_piece_at_target(game_state->board, "xxc1", KNG_W);
+                    game_board_set_piece_at_source(game_state->board, "e1xx", EMPTY);
+                    game_board_set_piece_at_target(game_state->board, "xxd1", TWR_W);
+                    game_board_set_piece_at_source(game_state->board, "a1xx", EMPTY);
+
+                    game_state->state = GAME_STATE_PLAYING;
+            }
+            game_draw_pre();
+
+            ClearBackground(LIGHTGRAY);
+            DrawTexture(game_state->background, 0, 0, WHITE);
+            
+            BeginMode3D(game_state->camera_perspective);
+            {
+                game_board_draw(game_state->board);
+                game_board_pieces_draw(KNG_W, game_state->piece_to_move_position_actual);
+                game_board_pieces_draw(TWR_W, game_state->piece_castling_position_actual);
+            }
+            EndMode3D();
+
+            BeginMode3D(game_state->camera_top);
+            {
+                game_board_draw(game_state->board);
+                game_board_pieces_draw(game_state->piece_to_move, game_state->piece_to_move_position_actual);
             }
             EndMode3D();
             game_draw_post(game_state);
